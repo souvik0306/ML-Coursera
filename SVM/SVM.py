4ac11cae0fbe3d7dd62ca72628ba527fa4ba2a60
Souvik Datta <souvikdatta123@gmail.com>
2022-02-02 10:20:21 +0530

    New SVM

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -5,9 +5,10 @@ from sklearn.datasets._samples_generator import make_blobs
 
 X,y = make_blobs(40,centers=2,random_state=20)
 
-clf = svm.SVC(kernel='linear',C=100)
-
+clf = svm.SVC(kernel='linear',C=1)
 clf.fit(X,y)
-
 plt.scatter(X[:,0],X[:,1],c=y,s=30,cmap=plt.cm.Paired)
-plt.show()
+plt.show() 
+
+newData = [[3,4],[5,6],[4,6]]
+print(clf.predict(newData))
