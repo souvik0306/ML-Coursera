eea8c829cdef8c3907e9070b1b0352762aa778cc
Souvik Datta <souvikdatta123@gmail.com>
2022-02-05 07:57:05 +0530

    Pandas For Udemy

--- SVM/SVM_plt.py
+++ SVM/SVM_plt.py
@@ -20,4 +20,5 @@ Z = clf.decision_function(xy).reshape(XX.shape)
 
 ax.contour(XX,YY,Z,colors = 'k',levels = [-1,0,1],alpha = 0.5,linestyles = ['--','-','--'])
 ax.scatter(clf.support_vectors_[:,0],clf.support_vectors_[:,1],s=100,linewidths=1,facecolors='none')
+
 plt.show()
